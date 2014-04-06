204b000479d78df04ce8116511ee1be795d3b7a2
Thomas <tberry@sandiego.edu>
2014-04-06 15:47:54 -0700

    Redid physics, added collision detection and ability to restart game (restart does not function properly, scoring still broken).

--- src/main/scala/Flappy/flap/flappy.scala
+++ src/main/scala/Flappy/flap/flappy.scala
@@ -9,8 +9,8 @@ import net.scage.ScageLib._
 
 object flappy extends ScageScreenApp("Project Flap", 800, 600){
   backgroundColor = BLACK
-val physics = ScagePhysics(gravity = Vec(0, 0))
-physics.addPhysical(flapper)
+  val physics = ScagePhysics(gravity = Vec(0, 0))
+  physics.addPhysical(flapper)
 
 //action {
   //if(flapper.velocity.x < 0) {
@@ -19,74 +19,114 @@ physics.addPhysical(flapper)
   //}
 //}
 
+  var collide = false
+  
+  keyIgnorePause(KEY_Q, onKeyDown = 
+  {
+   // if(onPause)
+     // {
+      restart()
+      pauseOff()
+      print("You Lose!", windowWidth/2, windowHeight/2, WHITE)
+      //}
+    })
+
+  action {
+    if(ObstacleCreator.botObstacles.length > 1) 
+      {if(((flapper.coord.x - 15 >= (ObstacleCreator.botObstacles.last.coord.x - 300) && flapper.coord.x - 15 <= (ObstacleCreator.botObstacles.last.coord.x - 250)) || (flapper.coord.x - 15>= (ObstacleCreator.botObstacles.last.coord.x -340) && flapper.coord.x - 15 <= (ObstacleCreator.botObstacles.last.coord.x - 290)))
+        && !(flapper.coord.y - 31 > ObstacleCreator.botObstacles.dropRight(1).last.coord.y && flapper.coord.y < (ObstacleCreator.botObstacles.dropRight(1).last.coord.y + 300))) collide = true
+  }}
+  
 action {
-    ObstacleCreator.score()
-    physics.step()
+    //ObstacleCreator.score()
+    //physics.step()
     ObstacleCreator.continue()
-    
+    if(collide)
+    {
+      pause()
+      deleteSelf()
     }
+    }
+
   
   interface{
-    if(!onPause) print((ObstacleCreator.point).toInt, windowWidth/2, windowHeight/2, WHITE)
+    if(!onPause) print((ObstacleCreator.point/150).toInt, windowWidth/2, windowHeight/2, WHITE)
 }
   }
 
 import flappy._
 
-object flapper extends DynaBall(Vec(400, 300), radius = 20, 1, false){
+object flapper extends DynaBox(Vec(400, 300), 40, 40, 1, false){
   val flap_image = image("sat.png", 40, 40,0,0,290,442)
   init {
     coord = Vec(400, 300)
-    velocity = Vec(0, 0)
+    val velocity = Vec(0, 0)
   }
   key(KEY_SPACE, onKeyDown = 
-      velocity += (Vec(0, 2000)), onKeyUp = velocity = (Vec(0, 0)))
-     
+      velocity += (Vec(0, 25)), onKeyUp = velocity = (Vec(0, 0)))
+ 
+  
+  
   render {
     drawDisplayList(flap_image, coord)
   }
   
-  action {
-    velocity += Vec(0, -17)
+action {
+    velocity += Vec(0, -.5)
+    coord += velocity
   }
 }
 
 object ObstacleCreator
 {
-  val obstacles = ArrayBuffer[Physical]()
+  val botObstacles = ArrayBuffer[Physical]()
+  val topObstacles = ArrayBuffer[Physical]()
   var point = 0
-  def addObstacle(obstacle:Physical)
+  def addtopObstacle(obstacle:Physical)
   {
     physics.addPhysical(obstacle)
-    obstacles += obstacle
-    if(obstacles.length > 10) removeObstacle()
+    topObstacles += obstacle
+    if(topObstacles.length > 10) removetopObstacle()
+    if(collide) topObstacles.remove(0, topObstacles.length-1)
+  }
+    def addbotObstacle(obstacle:Physical)
+  {
+    physics.addPhysical(obstacle)
+    botObstacles += obstacle
+    if(botObstacles.length > 10) removebotObstacle()
+    if(collide) botObstacles.remove(0, botObstacles.length-1)
+  }
+  def removetopObstacle()
+  {
+    val obstacle = topObstacles.remove(0)
+    physics.removePhysicals(obstacle)
   }
   
-  def removeObstacle()
+  def removebotObstacle()
   {
-    val obstacle = obstacles.remove(0)
+    val obstacle = botObstacles.remove(0)
     physics.removePhysicals(obstacle)
   }
   
   def continue()
   {
-    if(obstacles.length == 0) generateObstacles((math.random*600).toFloat)
-        else if(obstacles.last.coord.x < 500) generateObstacles((math.random*400).toFloat)
+    if(botObstacles.length == 0) generateObstacles((math.random*400).toFloat)
+        else if(botObstacles.last.coord.x < 500) generateObstacles((math.random*400).toFloat)
   }
   
-  def score()
-  {
-    if(obstacles.length < 2) point = 0
-    else if(obstacles.last.coord.x == 700) point += 1
-  }
+  //def score()
+  //{
+   // if(botObstacles.length < 2) point = 0
+   // else if(botObstacles.last.coord.x >= 650 && obstacles.last.coord.x <= 700) point += 1
+ // }
   
   def generateObstacles(height:Float)
   {
-    val secondY = (windowHeight - height - 200)
+    val secondY = (windowHeight - height - 300)
     val obst1 = new Obstacle(Vec(windowWidth, windowHeight), height)
-    val obst2 = new Obstacle(Vec(windowWidth, secondY), secondY)
-    addObstacle(obst1)
-    addObstacle(obst2)
+    val obst2 = new Obstacle(Vec(windowWidth, secondY), windowHeight)
+    addtopObstacle(obst1)
+    addbotObstacle(obst2)
   }
   }
 
@@ -94,7 +134,10 @@ class Obstacle(init_coord:Vec, height:Float) extends DynaBox(init_coord, 50, hei
 {
   init {
     coord = init_coord
-    velocity = Vec(-100, 0)
+    //velocity = Vec(-100, 0)
+  }
+  action {
+    coord += Vec(-1.5, 0)
   }
   render {
     drawRect(coord, this.box_width, this.box_height)}
